f88317fc4d799c12a50e0f032981c1f75befa3d4
sai_lokesh_29 <108468239+sai-lokesh-29@users.noreply.github.com>
2023-12-24 12:03:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,6 +80,10 @@ app.use((req, res, next) => {
     next();
 })
 
+app.get("/", (req, res) => {
+    res.redirect("/listings")
+})
+
 app.use("/listings", listingsRoute)
 
 app.use("/listings/:id/review", reviewsRoute)
